2220a4c932c6df6d6e08c3ded764add28497655e
Riya Gupta <126321833+Ratanvee@users.noreply.github.com>
2025-03-08 12:40:35 +0530

    Fixed line endings for build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 
 # Install Ghostscript on Render
 apt-get update && apt-get install -y ghostscript
+python manage.py migrate
+python manage.py collectstatic --noinput
+gunicorn app.wsgi:application --bind 0.0.0.0:8000
